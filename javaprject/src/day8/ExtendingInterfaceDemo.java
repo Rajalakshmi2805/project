00c5062ced27c484c6ed07718a4a3b00d1d9e466
Student <Student@TRAINLAB>
2024-07-18 15:41:49 +0530

    project

---
+++ javaprject/src/day8/ExtendingInterfaceDemo.java
@@ -0,0 +1,13 @@
+package day8;
+
+public class ExtendingInterfaceDemo implements ChildInterface {
+	public void print()
+	{
+		System.out.println("print method");
+	}
+	public void show()
+	{
+		System.out.println("shoe method");
+	}
+
+}
