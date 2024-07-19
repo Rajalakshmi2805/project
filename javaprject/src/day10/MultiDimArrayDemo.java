7891f7934f53cd8a21421353a186dd6b88b4d895
Student <Student@TRAINLAB>
2024-07-19 16:15:40 +0530

    project

---
+++ javaprject/src/day10/MultiDimArrayDemo.java
@@ -0,0 +1,12 @@
+package day10;
+
+public class MultiDimArrayDemo {
+
+	public static void main(String[] args) {
+		int c[][]= {{12,35},{15,43},{5,10}};
+		System.out.println("No. of Rows in c array:"+c.length);
+		MLArray.printArray(c);
+
+	}
+
+}
